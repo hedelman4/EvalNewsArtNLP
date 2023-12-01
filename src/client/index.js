52350adf8b4a45ff15cfa47b21ab0f6e7ca4759c
Harry <hte488@gmail.com>
2023-12-01 16:52:44 -0500

    retrieve and show meaningcloud api data

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,55 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+//import { checkForName } from './js/nameChecker'
+//import { handleSubmit } from './js/formHandler'
+let text = ''
+let score_tag = ''
+let subjectivity = ''
 
+/*
 console.log(checkForName);
-
 alert("I EXIST")
 console.log("CHANGE!!");
+*/
+
+const postData = async ( url = '', data = {})=>{
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data) 
+    });
+  
+    try {
+        const newData = await response.json();
+        return newData;
+    }
+    catch(error) {
+        console.log("error", error);
+    }
+};
+
+async function performAction() {
+    const response = await fetch('/all')
+    const data = await response.json()
+    const meanCloud = await fetch('https://api.meaningcloud.com/sentiment-2.1?key='+data['myKey']+'&lang=en&txt=sample')
+    const meanCloudData = await meanCloud.json()
+    text = document.getElementById('name').value
+    score_tag = meanCloudData['score_tag']
+    subjectivity = meanCloudData['subjectivity']
+    postData('/add', {text:text, score_tag:score_tag, subjectivity:subjectivity})
+    updateUI()
+}
+
+document.getElementById('submit').addEventListener('click', performAction);
+
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+        const allData = await request.json()
+        console.log(allData)
+        document.getElementById('results').innerHTML = allData.text + ' ' + allData.score_tag + ' ' + allData.subjectivity
+    }catch(error){
+      console.log("error", error);
+    }
+  }
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,27 @@
+require('dotenv').config();
+const myKey = process.env.MY_API_KEY
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key='
 var path = require('path')
 const express = require('express')
+const fetch = require('node-fetch');
 const mockAPIResponse = require('./mockAPI.js')
-
 const app = express()
-
+projectData = {};
+projectData['myKey'] = myKey;
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
+
 app.get('/', function (req, res) {
+    console.log('res success')
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('dist/index.html'))
+    // res.sendFile(path.resolve('dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -21,3 +32,18 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+app.get('/all', async function (req, res) {
+    res.send(projectData)
+});
+
+app.post('/add', addData);
+
+function addData(req,res) {
+    let data = req.body;
+    projectData['text'] = data.text;
+    projectData['score_tag'] = data.score_tag;
+    projectData['subjectivity'] = data.subjectivity;
+    console.log(projectData)
+    res.send(projectData)
+};
